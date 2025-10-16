0e35f7aacb3c265b5d6b52c014860bd055f4046e
London Park <ghpark@baeoom.com>
2025-10-17 08:51:06 +0900

    HEALTH-ROCK

--- server.ts
+++ server.ts
@@ -54,7 +54,7 @@ app.get("/hello", (c) => {
 });
 
 app.get("/health", (c) => {
-  return c.text("⭐️🎉✅ HONO: HELATH CHECK SUCCESS\n", 200);
+  return c.text("💗HEALTHY-ROCK ✅ HONO: HELATH CHECK SUCCESS\n", 200);
 });
 
 app.get("/api/users/:id", (c) => {
